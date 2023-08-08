3be335a67bdeb1961980f27b0408be130e79b5b4
Maca <maca@brutalrocks.com>
2023-08-08 16:51:57 -0400

    Add: mobile version

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,23 +10,6 @@ export default function Header() {
   return (
     <header className="header">
       <Navigation />
-      {/* <div className="header__info">
-        <Link to="/" className="header__logo-link">
-          <img className="header__logo" src={logo} alt="logo" />
-        </Link>
-
-        {location.pathname === "/" && (
-          <Link to="/jobs" className="header__link">
-            Trabajos TI
-          </Link>
-        )}
-
-        {location.pathname === "/jobs" && (
-          <Link to="/" className="header__link">
-            Sobre GreatJobs
-          </Link>
-        )}
-      </div> */}
       <hr className="header__line" />
     </header>
   );
